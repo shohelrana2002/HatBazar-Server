36bf77b80ce8b36c6d7b93a055e0c2f69986cdde
shohelrana2002 <rana2021tang@gmail.com>
2026-05-03 21:20:18 +0600

    Hatbazar Server Site Login Api Authentication Api routes db Connect Add

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { register, login } from "../controllers/authController.js";
 
 const authRouter = express.Router();
-
+// ghjjj
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 
