c23a303ebd0e12d81411abdabce363fcd24d7c09
shohelrana2002 <rana2021tang@gmail.com>
2026-06-14 23:09:42 +0600

    Hatbazar Server Site Login Api Authentication Api routes db Connect Add

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,7 +12,7 @@ export const createProduct = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
-// get all productsyyyyyyyyy
+// get all product
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find().sort({ createdAt: -1 });
